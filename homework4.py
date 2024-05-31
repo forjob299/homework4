8e7711906e5e0da646686e5a52fec81048705b6b
forjob299 <forjob299@gmail.com>
2024-05-31 17:33:16 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+my_string = input('Здравствуйте, user! Напишите о себе: ')
+print('Количество символов в ответе user:', len(my_string))
+print('Ответ user в верхнем регистре:', my_string.upper())
+print('Ответ user в нижнем регистре:', my_string.lower())
+print('Ответ user без пробелов:', my_string.replace(' ', ''))
+print('Первый символ из ответа user:', my_string[0])
+print('Последний символ из ответа user:', my_string[-1])
+
+
+
